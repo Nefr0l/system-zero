5af2712ba5f51594d1dd9affe6aef7c2ef6cf938
wrzosek <filip.wrzosek07@gmail.com>
2024-11-06 09:27:05 +0100

    init: basic project structure

---
+++ Assets/Scripts/Game Manager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using Classes;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    
+    public List<Module> modules = new List<Module>();
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
