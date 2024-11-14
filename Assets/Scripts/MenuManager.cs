a6a5fd44335ec1c068180e76ce20b4f17bad4197
wrzosek <filip.wrzosek07@gmail.com>
2024-11-14 19:01:38 +0100

    fix: add todo

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public void Exit()
+    {
+        Environment.Exit(0);
+    }
+}
---
+++ Assets/TODO.md
@@ -0,0 +1,9 @@
+# Features
+- [ ] Add functionality to main menu
+- [ ] Add level mechanic
+
+# Bugs
+- [ ] Fix graphics to have 4 colors
+
+# Other
+- [ ] Do some playtesting
