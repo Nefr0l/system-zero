a5415fa7d1e4e371b79215c74814896713daf044
wrzosek <filip.wrzosek07@gmail.com>
2024-11-20 20:58:40 +0100

    feat: add settings ui

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,11 +1,46 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
+    public List<GameObject> panels;
+
+    private void Start()
+    {
+        HidePanels();
+        InitializePanels();
+        ShowPanel(panels[0]);
+    }
+
     public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+    }
+
+    public void ShowPanel(GameObject panel)
+    {
+        HidePanels();
+        panel.SetActive(true);
+    }
+
+    private void InitializePanels()
+    {
+        Vector2 pos = GameObject.Find("Canvas").transform.position;
+        
+        foreach (var p in panels)
+        {
+            p.transform.position = pos;
+        }
+    }
+
+    private void HidePanels()
+    {
+        foreach (var p in panels)
+        {
+            p.SetActive(false);
+        }
     }
 }
