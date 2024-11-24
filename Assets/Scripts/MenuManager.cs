c6a2168b335884a3ce4e2909fee693f4850c23f1
wrzosek <filip.wrzosek07@gmail.com>
2024-11-24 15:25:11 +0100

    feat: change some audio, change menu layout, add button animations

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -7,6 +7,7 @@ using UnityEngine.SceneManagement;
 public class MenuManager : MonoBehaviour
 {
     public List<GameObject> panels;
+    public GameObject particlePrefab;
 
     private void Start()
     {
