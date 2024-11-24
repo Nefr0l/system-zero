9ced0d4cfaa1a555b85601407086c040d32e2952
wrzosek <filip.wrzosek07@gmail.com>
2024-11-24 15:43:40 +0100

    feat: add level 4

--- Assets/Scripts/ModuleMove.cs
+++ Assets/Scripts/ModuleMove.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class ModuleMove : MonoBehaviour
 {
     public GameObject Checkbox;
+    public bool CanMove = true;
     
     private float offset;
     private GameManager gameManager;
@@ -29,22 +30,26 @@ public class ModuleMove : MonoBehaviour
     {
         connectionManager.ShowLines();
         connectionManager.UpdateLines();
-    
-        // Object drag code remains the same
-        Vector2 cursorPoint = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
-        Vector2 cursorPosition = Camera.main.ScreenToWorldPoint(cursorPoint);
-    
-        float cursorX = cursorPosition.x;
-        float cursorY = cursorPosition.y;
-        Vector2 objectPosition = cursorPosition;
 
-        // Boundaries for object dragging
-        if (cursorX < gameManager.leftBorder + offset) objectPosition.x = gameManager.leftBorder + offset;
-        if (cursorX > gameManager.rightBorder - offset) objectPosition.x = gameManager.rightBorder - offset;
-        if (cursorY < gameManager.downBorder + offset) objectPosition.y = gameManager.downBorder + offset;
-        if (cursorY > gameManager.topBorder - offset) objectPosition.y = gameManager.topBorder - offset;
-
-        if (!GameManager.IsWin) transform.position = objectPosition;
+        if (CanMove)
+        {
+            // Object drag code remains the same
+                    Vector2 cursorPoint = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+                    Vector2 cursorPosition = Camera.main.ScreenToWorldPoint(cursorPoint);
+                
+                    float cursorX = cursorPosition.x;
+                    float cursorY = cursorPosition.y;
+                    Vector2 objectPosition = cursorPosition;
+            
+                    // Boundaries for object dragging
+                    if (cursorX < gameManager.leftBorder + offset) objectPosition.x = gameManager.leftBorder + offset;
+                    if (cursorX > gameManager.rightBorder - offset) objectPosition.x = gameManager.rightBorder - offset;
+                    if (cursorY < gameManager.downBorder + offset) objectPosition.y = gameManager.downBorder + offset;
+                    if (cursorY > gameManager.topBorder - offset) objectPosition.y = gameManager.topBorder - offset;
+            
+                    if (!GameManager.IsWin) transform.position = objectPosition;
+        }
+        
     }
 
 }
